a88385c2c4be19095141bcf1e85f87c49664fb75
crab409 <152848510+crab409@users.noreply.github.com>
2024-07-16 20:54:32 +0900

    기본 템플릿

---
+++ WEB/developer_info.js
@@ -0,0 +1,20 @@
+developer = [
+    ["홍유민", "202410520@dong-a.hs.kr", "yumin0878@naver.com"],
+    ["이승민", "202410520@dong-a.hs.kr"]
+]
+
+i = 0;
+while(i<developer.length) {
+    j = 0;
+    document.write('<li><ul>');
+    while(j<developer[i].length) {
+        if(j===0) {
+            document.write('<li><strong>'+developer[i][j]+'</strong></li>');
+        } else {
+            document.write('<li>'+developer[i][j]+'</li>');
+        }
+        j-=-1;
+    }
+    document.write('</ul></li>');
+    i-=-1;
+}
